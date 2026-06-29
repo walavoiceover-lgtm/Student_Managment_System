d9b7b300da5f7a15c6fdc17c74ac24fb82303ade
walavoiceover-lgtm <walavoiceover@gmail.com>
2026-06-29 20:33:53 +0300

    Add files via upload

---
+++ main.pi.py
@@ -0,0 +1,11 @@
+print("my name is:zaid")
+print("my field of studey : it")
+print("the year is :2026")
+
+print("my name is:dan")
+print("my field of studey : engener")
+print("the year is :2027")
+
+print("my name is:sara")
+print("my field of studey : medicine")
+print("the year is :2017")
